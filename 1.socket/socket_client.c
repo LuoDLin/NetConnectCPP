ccb264cf7aeb91c03b03e94dc39c5eac9ecd5168
LuoDLin <1115796874@qq.com>
2023-08-11 14:29:33 +0800

    socket 客户端

--- 1.socket/socket_client.c
+++ 1.socket/socket_client.c
@@ -9,16 +9,26 @@
 #define SERVER_PORT 8080
 #define SERVER_IP "127.0.0.1"
 
+void sys_err(const char *str){
+    perror(str);
+    exit(1);
+}
+
 int main(){
     // 创建套接字
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(client_fd == -1){
+        sys_err("socket create failed");
+    }
 
     // 连接服务器
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr.s_addr);
-    connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if(connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+        sys_err("connect failed");
+    }
 
     // 与服务器通信
     char buf[1024];
@@ -32,10 +42,18 @@ int main(){
         }
 
         // 将用户输入的数据发送给服务器
-        write(client_fd, buf, sizeof(buf));
+        if(write(client_fd, buf, sizeof(buf)) == -1){
+            sys_err("write failed");
+        }
 
         // 读取服务器回写的数据
         int read_len = read(client_fd, buf, sizeof(buf));
+        if(read_len == -1){
+            sys_err("read failed");
+        }else if(read_len == 0){
+            printf("server closed...\n");
+            break;
+        }
         buf[read_len] = '\0';
 
         // 打印服务器回写的数据
