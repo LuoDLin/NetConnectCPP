70997902afb58188f3a866a24405176a1f6da71b
LuoDLin <1115796874@qq.com>
2023-08-12 10:47:48 +0800

    socket 客户端、服务器

--- 1.socket/socket_client.c
+++ 1.socket/socket_client.c
@@ -14,26 +14,78 @@ void sys_err(const char *str){
     exit(1);
 }
 
-int main(){
-    // 创建套接字
-    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(client_fd == -1){
+int Socket(int domain, int type, int protocol){
+    int fd = socket(domain, type, protocol);
+    if(fd == -1){
         sys_err("socket create failed");
     }
+    return fd;
+}
+
+int Setsockopt(int fd, int level, int optname,const void *optval, socklen_t optlen){
+    int ret = setsockopt(fd, level, optname, optval, optlen);
+    if(ret == -1){
+        sys_err("setsockopt failed");
+    }
+    return ret;
+}
+
+int Bind (int fd, __CONST_SOCKADDR_ARG addr, socklen_t len){
+    int ret = bind(fd, addr, len);
+    if(ret == -1){
+        sys_err("bind failed");
+    }
+    return ret;
+}
+
+int Listen(int fd, int backlog){
+    int ret = listen(fd, backlog);
+    if(ret == -1){
+        sys_err("listen failed");
+    }
+    return ret;
+}
+
+int Accept(int fd, __SOCKADDR_ARG addr, socklen_t *len){
+    int ret = accept(fd, addr, len);
+    if(ret == -1){
+        sys_err("accept failed");
+    }
+    return ret;
+}
+
+int Read(int fd, void *buf, size_t count){
+    int ret = read(fd, buf, count);
+    if(ret == -1){
+        sys_err("read failed");
+    }
+    ((char*)buf)[ret] = '\0';
+    return ret;
+}
+
+int Connect(int fd, __CONST_SOCKADDR_ARG addr, socklen_t len){
+    int ret = connect(fd, addr, len);
+    if(ret == -1){
+        sys_err("connect failed");
+    }
+    return ret;
+}
+
+int main(){
+    // 创建套接字
+    int client_fd = Socket(AF_INET, SOCK_STREAM, 0);
 
     // 连接服务器
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr.s_addr);
-    if(connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
-        sys_err("connect failed");
-    }
+    Connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
     // 与服务器通信
     char buf[1024];
     while(1){
-        // 读取用户输入
+
         printf("input: ");
         scanf("%s", buf);
 
@@ -42,21 +94,15 @@ int main(){
         }
 
         // 将用户输入的数据发送给服务器
-        if(write(client_fd, buf, sizeof(buf)) == -1){
-            sys_err("write failed");
-        }
+        write(client_fd, buf, sizeof(buf));
 
         // 读取服务器回写的数据
-        int read_len = read(client_fd, buf, sizeof(buf));
-        if(read_len == -1){
-            sys_err("read failed");
-        }else if(read_len == 0){
+        int read_len = Read(client_fd, buf, sizeof(buf));
+        if(read_len == 0){
             printf("server closed...\n");
             break;
         }
-        buf[read_len] = '\0';
-
-        // 打印服务器回写的数据
+        
         printf("recv buf: %s\n", buf);
     }
 
--- 1.socket/socket_server.c
+++ 1.socket/socket_server.c
@@ -12,69 +12,95 @@ void sys_err(const char *str){
     exit(1);
 }
 
-int main(){
-    // 创建套接字
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_fd == -1){
+int Socket(int domain, int type, int protocol){
+    int fd = socket(domain, type, protocol);
+    if(fd == -1){
         sys_err("socket create failed");
     }
+    return fd;
+}
+
+int Setsockopt(int fd, int level, int optname,const void *optval, socklen_t optlen){
+    int ret = setsockopt(fd, level, optname, optval, optlen);
+    if(ret == -1){
+        sys_err("setsockopt failed");
+    }
+    return ret;
+}
+
+int Bind (int fd, __CONST_SOCKADDR_ARG addr, socklen_t len){
+    int ret = bind(fd, addr, len);
+    if(ret == -1){
+        sys_err("bind failed");
+    }
+    return ret;
+}
+
+int Listen(int fd, int backlog){
+    int ret = listen(fd, backlog);
+    if(ret == -1){
+        sys_err("listen failed");
+    }
+    return ret;
+}
+
+int Accept(int fd, __SOCKADDR_ARG addr, socklen_t *len){
+    int ret = accept(fd, addr, len);
+    if(ret == -1){
+        sys_err("accept failed");
+    }
+    return ret;
+}
+
+int Read(int fd, void *buf, size_t count){
+    int ret = read(fd, buf, count);
+    if(ret == -1){
+        sys_err("read failed");
+    }
+    ((char*)buf)[ret] = '\0';
+    return ret;
+}
+
+int main(){
+    // 创建套接字
+    int server_fd = Socket(AF_INET, SOCK_STREAM, 0);
 
     // 设置地址重用
     int optval = 1;
-    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1){
-        sys_err("setsockopt failed:");
-    }
+    Setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
 
     // 绑定地址结构
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if(bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
-        sys_err("bind failed:");
-    }
+    Bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
     // 监听
-    if(listen(server_fd, 128) == -1){
-        sys_err("listen failed:");
-    }
+    Listen(server_fd, 128);
 
     printf("server is running...\n");
-
-    // 接收客户端连接
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
-    if(client_fd == -1){
-        sys_err("accept failed:");
-    }
-    
-    // 打印客户端的 ip 和 port
-    char ip[64];
-    printf("client ip: %s, port: %d\n", 
-        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, ip, sizeof(ip)), 
-        ntohs(client_addr.sin_port));
-    
-    char buf[1024];
     while(1){
-        // 读取客户端发送的数据 read/recv
-        // recv(client_fd, buf, sizeof(buf), 0);
-        int len = read(client_fd, buf, sizeof(buf));
-        buf[len] = '\0';
+        int client_fd = Accept(server_fd, NULL, NULL);
+        printf("client connected...\n");
+        char buf[1024];
+        while(1){
+            // 读取客户端发送的数据 read/recv
+            // recv(client_fd, buf, sizeof(buf), 0);
+            int len = Read(client_fd, buf, sizeof(buf));
 
-        if(len == -1 ){  // 读取出错
-            sys_err("read failed:");
-        }else if(len == 0 ){
-            printf("client closed...\n");
-            break;
-        }else{  // 读取成功
-            printf("recv buf: %s\n", buf);  // 打印客户端发送的数据
-            write(client_fd, buf, len);     // 将数据回写给客户端
+           if(len == 0 ){
+                printf("client closed...\n");
+                break;
+            }else{
+                printf("recv buf: %s\n", buf);  // 打印客户端发送的数据
+                write(client_fd, buf, len);     // 将数据回写给客户端
+            }
         }
+        close(client_fd);
     }
 
     // 关闭套接字
-    close(client_fd);
     close(server_fd);
     return 0;
 }
